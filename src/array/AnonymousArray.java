fab501a50b1b11e4a39033831fe4b836b2b30534
arifur946954 <arif.bd08060@gmail.com>
2023-11-23 16:19:11 +0600

    passing anonymous array to method

---
+++ src/array/AnonymousArray.java
@@ -0,0 +1,18 @@
+package array;
+
+public class AnonymousArray {
+    //In Java, an anonymous array is an array that is created and initialized without explicitly specifying its name
+    public static void main(String[] args) {
+        //declear annynimous araay
+
+        printArray(new int[]{1,5,6,8,1,3,5,121,3});
+
+    }
+
+    public static void printArray(int num[]){
+        for (int array:num) {
+            System.out.println(array);
+
+        }
+    }
+}
--- src/array/PassArrayInMethod.java
+++ src/array/PassArrayInMethod.java
@@ -1,24 +1,24 @@
-package array;
-
-public class PassArrayInMethod {
-    //get the minimum number of an array using a method
-    public static void main(String[] args) {
-        int []num={33,5,2,22,11};
-        minimum(num);
-
-    }
-
-    private static void minimum(int[] num) {
-        int min=num[0];//33
-        for (int i=0;i<num.length-1;i++){
-            if (min>num[i+1]){
-                min=num[i+1];
-            }
-
-
-        }
-        System.out.println("Minimum Output is: "+min);
-
-
-    }
-}
+//package array;
+//
+//public class PassArrayInMethod {
+//    //get the minimum number of an array using a method
+//    public static void main(String[] args) {
+//        int []num={33,5,2,22,11};
+//        minimum(num);
+//
+//    }
+//
+//    private static void minimum(int[] num) {
+//        int min=num[0];//33
+//        for (int i=0;i<num.length-1;i++){
+//            if (min>num[i+1]){
+//                min=num[i+1];
+//            }
+//
+//
+//        }
+//        System.out.println("Minimum Output is: "+min);
+//
+//
+//    }
+//}
