1e667bbe1db41c6f49e6f845da3f28f096236c95
arifur946954 <arif.bd08060@gmail.com>
2023-11-23 17:04:41 +0600

    add two array

---
+++ src/array/AddItion.java
@@ -0,0 +1,28 @@
+package array;
+
+import java.util.Arrays;
+
+public class AddItion {
+    public static void main(String[] args) {
+        int[] num1={1,2,3,5,4};
+        int[] num2={1,2,3,5,4};
+        int[] num3=new int[5];
+
+        addition(num1,num2,num3);
+
+    }
+
+    private static void addition(int[] num1, int[] num2,int[] num3) {
+        for (int i=0; i< num1.length;i++){
+            num3[i]=num1[i]+num2[i];
+
+
+
+        }
+        System.out.println("output is "+Arrays.toString(num3));
+
+
+
+    }
+
+}
