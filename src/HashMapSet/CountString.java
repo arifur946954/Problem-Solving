af5e0275e615b57182f17efa31b5b0e9a075bcdf
arifur946954 <arif.bd08060@gmail.com>
2023-11-30 18:29:40 +0600

    string count

---
+++ src/HashMapSet/CountString.java
@@ -0,0 +1,30 @@
+package HashMapSet;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class CountString {
+    public  static void getCharCount(String name){
+        HashMap<Character,Integer> hNmae=new HashMap<Character, Integer>();
+       String lowName=name.toLowerCase();
+       char[] arrStr=lowName.toCharArray();
+        for (char c: arrStr){
+            //containKey return true or false
+            if (hNmae.containsKey(c)){
+                hNmae.put(c,hNmae.get(c)+1);
+            }
+            else {
+                hNmae.put(c,1);
+            }
+        }
+        System.out.println(hNmae);
+    }
+
+    public static void main(String[] args) {
+
+getCharCount("Hello World");
+
+
+
+    }
+}
---
+++ src/HashMapSet/HashMapX.java
@@ -0,0 +1,28 @@
+//package HashMapSet;
+//
+//import java.util.HashMap;
+//
+//public class HashMapX {
+//    public static void main(String[] args) {
+//        HashMap<Integer,String> countryList=new HashMap<Integer,String>();
+//        countryList.put(1,"Bangaladesh");
+//        countryList.put(2,"pakistan");
+//        countryList.put(3,"germany");
+//
+//        System.out.println(countryList);
+//        //print 3th element
+//        System.out.println(countryList.get(3));
+//        //print all element of value
+//        for (String x:countryList.values()){
+//            System.out.println(x);
+//        }
+//
+//
+//        //print all first element
+//        for (int x:countryList.keySet()){
+//            System.out.println(x);
+//        }
+//
+//
+//    }
+//}
